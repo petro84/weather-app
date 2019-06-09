6585d8653c2822b964f4160a25e036fe5c8701ec
Larry Petroski <larry@Petro.local>
2019-06-08 21:04:09 -0500

    Prod prep

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDir = path.join(__dirname, '../public')
 const viewsDir = path.join(__dirname, '../templates/views')
@@ -81,4 +82,4 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(port, () => console.log(`Server running on port ${port}`))
